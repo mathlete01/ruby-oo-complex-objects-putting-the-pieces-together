0c92c12a9e8591231d064b7f8a3c127e9558d5f2
mathlete01 <msallin@gmail.com>
2020-10-06 21:53:37 -0700

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,46 @@
+class Book
 
+    #attr_reader :title, :author
+    #attr_accessor :author
 
+    #writers
+    def author=(author_param)
+        @author = author_param  
+    end
+
+    def page_count=(page_count_param)
+        @page_count = page_count_param
+    end
+
+    def genre=(genre_param)
+        @genre = genre_param
+    end
+
+    #readers
+    def author
+        @author
+    end
+
+    def title
+        @title
+    end
+
+    def page_count
+        @page_count
+    end
+
+    def genre
+        @genre
+    end
+    
+    def initialize(title)
+        @title = title
+        @author = ""
+        @page_count = 272
+    end
+
+   def turn_page
+        puts "Flipping the page...wow, you read fast!"
+   end
+
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,55 @@
 # Make your shoe class here!
+
+class Shoe
+    
+    def initialize(brand)
+        @brand = brand
+    end
+
+    #readers
+    def brand
+        @brand  
+    end
+
+    def color
+        @color  
+    end
+
+    def size
+        @size  
+    end
+
+    def material
+        @material  
+    end
+
+    def condition
+        @condition
+    end
+
+    #writers
+
+    def color=(color_param)
+        @color = color_param
+    end
+
+    def size=(size_param)
+        @size = size_param
+    end
+
+    def material=(material_param)
+        @material = material_param
+    end
+
+    def condition=(condition_param)
+        @condition = condition_param
+    end
+
+    #methods
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+
+end
