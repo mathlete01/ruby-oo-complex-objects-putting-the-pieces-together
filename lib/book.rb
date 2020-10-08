88ad25901c220745c1ce1c46465862d4c78c9686
mathlete01 <msallin@gmail.com>
2020-10-08 15:06:08 -0700

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,46 +1,45 @@
 class Book
 
-    #attr_reader :title, :author
-    #attr_accessor :author
+  #attr_reader :title, :author
+  #attr_accessor :author
 
-    #writers
-    def author=(author_param)
-        @author = author_param  
-    end
+  #writers
+  def author=(author_param)
+    @author = author_param
+  end
 
-    def page_count=(page_count_param)
-        @page_count = page_count_param
-    end
+  def page_count=(page_count_param)
+    @page_count = page_count_param
+  end
 
-    def genre=(genre_param)
-        @genre = genre_param
-    end
+  def genre=(genre_param)
+    @genre = genre_param
+  end
 
-    #readers
-    def author
-        @author
-    end
+  #readers
+  def author
+    @author
+  end
 
-    def title
-        @title
-    end
+  def title
+    @title
+  end
 
-    def page_count
-        @page_count
-    end
+  def page_count
+    @page_count
+  end
 
-    def genre
-        @genre
-    end
-    
-    def initialize(title)
-        @title = title
-        @author = ""
-        @page_count = 272
-    end
+  def genre
+    @genre
+  end
 
-   def turn_page
-        puts "Flipping the page...wow, you read fast!"
-   end
+  def initialize(title)
+    @title = title
+    @author = ""
+    @page_count = 272
+  end
 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 end
